54e9e59ce514c83a58f86037fada7daf946dd8de
Oleksii Lesiuk <alexeylesiuk@gmail.com>
2019-09-18 18:22:46 +0300

    Create test-github-webhooks-pr-health.groovy

---
+++ test-github-webhooks-pr-health.groovy
@@ -0,0 +1,20 @@
+multibranchPipelineJob('NewCodes/test-github-webhooks-pr-health') {
+    branchSources {
+        github {
+            id('894e0368-9b9e-4d14-992e-5caeb3bc8a58')
+            scanCredentialsId('alexmelkor_github_credentials')
+            repoOwner('alexmelkor')
+            repository('test-github-webhooks')
+            buildOriginBranchWithPR(true)
+            buildOriginBranch(false)
+            buildForkPRHead(false)
+            buildForkPRMerge(false)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(-1)
+            numToKeep(7)
+        }
+    }
+}
