ee4cc51d7732e169583a0748555a838f65e2eccd
Oleksii Lesiuk <alexeylesiuk@gmail.com>
2019-09-18 18:27:00 +0300

    Update and rename test-github-webhooks-pr-health.groovy to test_github_webhooks_pr_health.groovy

--- test_github_webhooks_pr_health.groovy
+++ test_github_webhooks_pr_health.groovy
@@ -1,4 +1,4 @@
-multibranchPipelineJob('NewCodes/test-github-webhooks-pr-health') {
+multibranchPipelineJob('test-github-webhooks-pr-health') {
     branchSources {
         github {
             id('894e0368-9b9e-4d14-992e-5caeb3bc8a58')
